4696f6f02a31981979f2e2c1a5ac0c7538c1edc3
Rodrigo Riveros-Vanegas <rodmtl@outlook.es>
2025-03-28 21:28:50 -0400

    update main page 1

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main>
       <div>Next.js on GitHub Pages</div>
+      <div>This is a next.js page hosted in github pages.</div>
     </main>
   );
 }
